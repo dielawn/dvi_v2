97c659fb8cb4784a2d04a54c8beb8c57164b87d0
dielawn <dielawnmerkill@gmail.com>
2025-04-20 08:01:56 -0600

    dvi component

---
+++ src/components/DVI.jsx
@@ -0,0 +1,13 @@
+import { Inspection, InspectionItem } from "../models/Inspection"
+
+const DVI = () => {
+
+    return (
+        //create Inspection form that lets users update InspectionItem status and notes
+        <div>
+            
+        </div>
+    )
+}
+
+export default DVI
